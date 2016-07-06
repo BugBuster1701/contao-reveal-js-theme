2ec7a87f746586cffd076cae61a784b180677a5f
Glen Langer <contao.modul@glen-langer.de>
2016-07-06 21:23:13 +0200

    Fixed #3, Transifex support

---
+++ module/languages/en/tl_layout.php
@@ -0,0 +1,5 @@
+<?php
+
+
+$GLOBALS['TL_LANG']['tl_layout']['useRevealJs'][0] = 'Reveal.js framework';
+$GLOBALS['TL_LANG']['tl_layout']['useRevealJs'][1] = 'Here you can activate the components of the reveal.js presentation framework.';
