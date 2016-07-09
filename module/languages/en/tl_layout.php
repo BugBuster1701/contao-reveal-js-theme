d19aec4abf0a64ab559be4b03769419059648182
Glen Langer <contao.modul@glen-langer.de>
2016-07-09 02:21:17 +0200

    Fixed #10,  Übersetzungen vervollständigen

--- module/languages/en/tl_layout.php
+++ module/languages/en/tl_layout.php
@@ -4,3 +4,99 @@
 $GLOBALS['TL_LANG']['tl_layout']['useRevealJs'][0] = 'Reveal.js framework';
 $GLOBALS['TL_LANG']['tl_layout']['useRevealJs'][1] = 'Here you can activate the components of the reveal.js presentation framework.';
 $GLOBALS['TL_LANG']['tl_layout']['revealJs_legend'] = 'Reveal.js framework options';
+
+
+
+$GLOBALS['TL_LANG']['tl_layout']['revealJsTheme'][0] = 'Theme';
+$GLOBALS['TL_LANG']['tl_layout']['revealJsTheme'][1] = 'Selection of  theme';
+
+
+$GLOBALS['TL_LANG']['tl_layout']['revealJsSize'][0] = 'Presentation Size';
+$GLOBALS['TL_LANG']['tl_layout']['revealJsSize'][1] = 'Overwrite the "normal" size of the presentation (width/height). Default 960/700. The framework will automatically scale presentations uniformly based on this size to ensure that everything fits on any given display or viewport.';
+
+
+$GLOBALS['TL_LANG']['tl_layout']['revealJsMargin'][0] = 'Margin';
+$GLOBALS['TL_LANG']['tl_layout']['revealJsMargin'][1] = 'Overwrite the factor of the display size that should remain empty around the content. Default: 0.1';
+
+
+$GLOBALS['TL_LANG']['tl_layout']['revealJsScale'][0] = 'Scale';
+$GLOBALS['TL_LANG']['tl_layout']['revealJsScale'][1] = 'Overwrite the bounds for smallest/largest possible scale to apply to content. Default: 0.2/1.5';
+
+
+
+$GLOBALS['TL_LANG']['tl_layout']['revealJsControls'][0] = 'Controls';
+$GLOBALS['TL_LANG']['tl_layout']['revealJsControls'][1] = 'Enables the controls in the bottom right corner';
+
+$GLOBALS['TL_LANG']['tl_layout']['revealJsProgress'][0] = 'Progress bar';
+$GLOBALS['TL_LANG']['tl_layout']['revealJsProgress'][1] = 'Enables the presentation progress bar';
+
+$GLOBALS['TL_LANG']['tl_layout']['revealJsSlideNumber'][0] = 'Page number';
+$GLOBALS['TL_LANG']['tl_layout']['revealJsSlideNumber'][1] = 'Enables the display of the page number of the current slide';
+
+$GLOBALS['TL_LANG']['tl_layout']['revealJsHistory'][0] = 'Browser history';
+$GLOBALS['TL_LANG']['tl_layout']['revealJsHistory'][1] = 'Push each slide change to the browser history';
+
+$GLOBALS['TL_LANG']['tl_layout']['revealJsKeyboard'][0] = 'Keyboard shortcuts';
+$GLOBALS['TL_LANG']['tl_layout']['revealJsKeyboard'][1] = 'Enables keyboard shortcuts for navigation';
+
+$GLOBALS['TL_LANG']['tl_layout']['revealJsOverview'][0] = 'Overview mode';
+$GLOBALS['TL_LANG']['tl_layout']['revealJsOverview'][1] = 'Enables the slide overview mode';
+
+$GLOBALS['TL_LANG']['tl_layout']['revealJsCenter'][0] = 'Vertical centering';
+$GLOBALS['TL_LANG']['tl_layout']['revealJsCenter'][1] = 'Enables vertical centering of slides';
+
+$GLOBALS['TL_LANG']['tl_layout']['revealJsTouch'][0] = 'Touch navigation';
+$GLOBALS['TL_LANG']['tl_layout']['revealJsTouch'][1] = 'Enables touch navigation on devices with touch input';
+
+
+$GLOBALS['TL_LANG']['tl_layout']['revealJsLoop'][0] = 'Loop';
+$GLOBALS['TL_LANG']['tl_layout']['revealJsLoop'][1] = 'Enables the loop of the presentation';
+
+
+$GLOBALS['TL_LANG']['tl_layout']['revealJsRtl'][0] = 'Direction';
+$GLOBALS['TL_LANG']['tl_layout']['revealJsRtl'][1] = 'Change the presentation direction to be RTL';
+
+
+$GLOBALS['TL_LANG']['tl_layout']['revealJsFragments'][0] = 'Fragments';
+$GLOBALS['TL_LANG']['tl_layout']['revealJsFragments'][1] = 'Turns fragments on and off globally';
+
+
+$GLOBALS['TL_LANG']['tl_layout']['revealJsEmbedded'][0] = 'Embedded mode';
+$GLOBALS['TL_LANG']['tl_layout']['revealJsEmbedded'][1] = 'Flags if the presentation is running in an embedded mode, i.e. contained within a limited portion of the screen';
+
+
+$GLOBALS['TL_LANG']['tl_layout']['revealJsAutoSlide'][0] = 'Auto-sliding';
+$GLOBALS['TL_LANG']['tl_layout']['revealJsAutoSlide'][1] = 'Number of milliseconds between automatically proceeding to the next slide, disabled when set to 0';
+
+
+$GLOBALS['TL_LANG']['tl_layout']['revealJsAutoSlideStoppable'][0] = 'Auto-sliding stoppable';
+$GLOBALS['TL_LANG']['tl_layout']['revealJsAutoSlideStoppable'][1] = 'Stop auto-sliding after user input';
+
+
+$GLOBALS['TL_LANG']['tl_layout']['revealJsMouseWheel'][0] = 'Mouse wheel';
+$GLOBALS['TL_LANG']['tl_layout']['revealJsMouseWheel'][1] = 'Enables slide navigation via mouse wheel';
+
+
+$GLOBALS['TL_LANG']['tl_layout']['revealJsHideAddressBar'][0] = 'Hide address bar';
+$GLOBALS['TL_LANG']['tl_layout']['revealJsHideAddressBar'][1] = 'Hides the address bar on mobile devices';
+
+
+$GLOBALS['TL_LANG']['tl_layout']['revealJsPreviewLinks'][0] = 'Preview links';
+$GLOBALS['TL_LANG']['tl_layout']['revealJsPreviewLinks'][1] = 'Opens links in an iframe preview overlay';
+
+
+$GLOBALS['TL_LANG']['tl_layout']['revealJsTransition'][0] = 'Transition style';
+$GLOBALS['TL_LANG']['tl_layout']['revealJsTransition'][1] = 'Selection of transition style';
+
+
+$GLOBALS['TL_LANG']['tl_layout']['revealJsTransitionSpeed'][0] = 'Transition speed';
+$GLOBALS['TL_LANG']['tl_layout']['revealJsTransitionSpeed'][1] = 'Selection of transition speed';
+
+
+$GLOBALS['TL_LANG']['tl_layout']['revealJsBackgroundTransition'][0] = 'Background transition style';
+$GLOBALS['TL_LANG']['tl_layout']['revealJsBackgroundTransition'][1] = 'Selection of transition style for full page slide backgrounds';
+
+
+$GLOBALS['TL_LANG']['tl_layout']['revealJsViewDistance'][0] = 'View Distance';
+$GLOBALS['TL_LANG']['tl_layout']['revealJsViewDistance'][1] = 'Define the number of slides away from the current that are visible';
+
