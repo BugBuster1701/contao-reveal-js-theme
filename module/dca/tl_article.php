c513e1fdfcca2a6107b9bd05e1bc19088b09bb40
Glen Langer <contao.modul@glen-langer.de>
2016-08-11 00:39:19 +0200

    Hook testweise Deaktiviert, nicht mehr in Nutzung

--- module/dca/tl_article.php
+++ module/dca/tl_article.php
@@ -1,9 +1,9 @@
 <?php
 
-$GLOBALS['TL_DCA']['tl_article']['config']['onload_callback'][] = array(
-    'BugBuster\RevealJs\Theme\Hooks',
-    'loadArticleDca'
-);
+//$GLOBALS['TL_DCA']['tl_article']['config']['onload_callback'][] = array(
+//    'BugBuster\RevealJs\Theme\Hooks',
+//    'loadArticleDca'
+//);
 
 $GLOBALS['TL_DCA']['tl_article']['list']['label']['reveal_original_label_callback'] =
     $GLOBALS['TL_DCA']['tl_article']['list']['label']['label_callback'];
